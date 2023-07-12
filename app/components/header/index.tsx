518c2c83a879524e119598415e5abc04e8d51580
Roz <83481681+SanxRoz@users.noreply.github.com>
2023-07-12 02:49:49 +0000

    v2

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,29 +1,36 @@
 "use client";
 
-import { PlugsConnected, ArrowRight } from "@phosphor-icons/react";
+import { Atom, ArrowRight } from "@phosphor-icons/react";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <>
-      <div className="flex flex-col items-center gap-9 max-w-[768px]">
-        <PlugsConnected
-          size={100}
-          color="#222"
-          weight="duotone"
-          className="w-16 h-16 md:w-20 md:h-20"
-        />
+      <div className="py-16 flex flex-col items-center w-full magicpattern">
+        <div className="flex flex-col items-center gap-5 max-w-[768px]">
+          <Atom
+            size={100}
+            color="#222"
+            weight="duotone"
+            className="w-16 h-16 md:w-20 md:h-20"
+          />
 
-        <div className="flex flex-col items-center gap-6">
-          <h1 className="header_text md:text-7xl text-3xl font-bold text-center">
-            Connect your business with AI in seconds
-          </h1>
-          <p className="w-[70%] text-center text-black/50">
-            Integrate and automate every task of your business with AI and
-            double your revenue in 3 months
-          </p>
-          <div className="flex flex-row gap-4">
-            <button className="flex flex-row gap-2 items-center font-medium bg-[#171717] text-white px-4 py-1 rounded-[10px]">Get Started <ArrowRight size={24} /></button>
-            <button className="bg-black/10">Contact Us</button>
+          <div className="flex flex-col items-center gap-6">
+            <h1 className="header_text md:text-7xl text-3xl font-bold text-center">
+              Connect your business with AI in seconds
+            </h1>
+            <p className="w-[70%] text-center text-black/50">
+              Integrate and automate every task of your business with AI and
+              double your revenue in 3 months
+            </p>
+            <div className="flex flex-row gap-4">
+              <button className="flex flex-row gap-2 items-center font-medium bg-[#171717] text-white px-4 py-1 rounded-[10px]">
+                Get Started <ArrowRight size={24} />
+              </button>
+              <button className="bg-black/10 px-4 py-1 rounded-[10px] font-medium">
+                Contact Us
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Header from "./components/header/index";
 
 export default function Home() {
   return (
-    <main className="flex text-lg min-h-screen flex-col items-center justify-between pt-32">
+    <main className="flex text-lg min-h-screen flex-col items-center justify-between pt-16">
       <Header />
     </main>
   );
